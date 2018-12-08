27eaf70166e73646a2064e1ff143ba80cde1668c
adidi3 <ap0413@bs.amalnet.k12.il>
2018-12-08 22:22:26 +0200

    1st commit

--- app/src/main/java/com/example/adipe/files/MainActivity.java
+++ app/src/main/java/com/example/adipe/files/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     InputStream is;
     String file = "file.txt";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
